0b1ac2945162a32c596b8692d6fa858dc07fa0bc
amandhimank <ak5532208@gmail.com>
2024-10-18 00:06:01 +0530

    added new url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-mern-0fyl.onrender.com",
     credentials: true
 }));
 

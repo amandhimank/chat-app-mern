2bc52c9922edc60308e30497e54474fc71ab24c6
amandhimank <ak5532208@gmail.com>
2024-10-18 00:21:45 +0530

    added socket url

--- frontend/utils/context/SocketContext.jsx
+++ frontend/utils/context/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:3000", {
+            const socket = io("https://chat-app-mern-0fyl.onrender.com", {
                 query: {
                     userId: authUser.user._id
                 }

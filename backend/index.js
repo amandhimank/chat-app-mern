cfa717a8e5658ed5ddabd03b3f350dd0b4457e23
amandhimank <ak5532208@gmail.com>
2024-10-17 23:59:25 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import path from 'path';
 
 import db from './db/connection.js';
 import { app, server } from './socket/socket.js';
+
+const _dirname = path.resolve();
 
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
@@ -18,10 +21,6 @@ app.use(cors({
     credentials: true
 }));
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World");
-// });
-
 import authRoutes from './routes/auth.routes.js'; 
 import messageRoutes from './routes/message.route.js';
 import userRoutes from './routes/user.routes.js';
@@ -29,6 +28,11 @@ import userRoutes from './routes/user.routes.js';
 app.use("/auth", authRoutes);
 app.use("/message", messageRoutes);
 app.use("/user", userRoutes);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     console.log("listening on port " + PORT);

a8a1941f1f3aaab113afe97acc437ae4cad771d4
amandhimank <ak5532208@gmail.com>
2024-10-19 02:46:51 +0530

    fixed ui

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -15,7 +15,7 @@ function Messages() {
   }, [messages]);
 
   return (
-    <div className='px-4 flex-1 overflow-auto'>
+    <div className='px-4 flex-1 overflow-auto w-full'>
         {!loading && messages.length > 0 && (
           messages.map((message) => (
             <div key={message._id} ref={lastMessageRef} >
